fc141bacefbf8cf7ae0135d880ec99a2287c8fb3
Salah-M <66308009+Salah-M@users.noreply.github.com>
2020-10-31 18:22:42 +0200

    Update student.h

--- Classes/student.h
+++ Classes/student.h
@@ -7,13 +7,12 @@ class Student: protected User
 {
 public:
 	vector<course> student_courses;
-    Student(string f,string l,date dv,vector<string>courses);
-	void sign_up();
+    	Student(string f,string l,date dv,vector<string>courses);
 	void sign_in(user_id, string password);
-	void create_id();
 	void register_course(course_id);
 	void add_interjection(movie_id,time);
 	void recommended();
+	void feedback(Professor p);
 };
 
 #endif // STUDENT_H
