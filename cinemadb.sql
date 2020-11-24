47d82fc8d242b76dc450bd45fd1b1144a9723b94
abdallah101 <abdallah1012@outlook.com>
2020-11-24 20:43:18 +0200

    fixed database table

--- cinemadb.sql
+++ cinemadb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 24, 2020 at 04:28 PM
+-- Generation Time: Nov 24, 2020 at 06:42 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -22,6 +22,32 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `cinemadb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `cinemadb`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE IF NOT EXISTS `courses` (
+  `courseID` int(10) NOT NULL AUTO_INCREMENT,
+  `courseName` varchar(25) NOT NULL,
+  `professorID` int(10) NOT NULL,
+  `syllabus` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`courseID`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`courseID`, `courseName`, `professorID`, `syllabus`) VALUES
+(1, 'EECE435', 0, 'test'),
+(2, 'Course Name: EECE435', 0, 'Description: asd'),
+(3, 'aa', 0, 'aa'),
+(4, 'asd', 0, 'asd'),
+(5, 'new', 0, 'new');
 
 -- --------------------------------------------------------
 
