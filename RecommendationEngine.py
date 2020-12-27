bf85cdfb382a689e3888f76d43b9eff313edbd62
Salah-M <balrogg1999@gmail.com>
2020-12-27 15:33:40 +0200

    added to RecommendationEngine

--- RecommendationEngine.py
+++ RecommendationEngine.py
@@ -24,12 +24,18 @@ class RecommendationEngine:
     
     def LoadRecommendations(self,user: User):
         Courses=user.courses
+        tags=[]
         faculty = []
         typ=[]
         cids=[]
         for i,j in Courses:
             
             temp=self.manager.getCourseinfo(str(j))
+            Movies=self.moviemanager.getCourseMovieID(str(j))
+            if(len(Movies)!=0):
+                tags.append(Movies[0][1])
+                tags.append(Movies[0][2])
+                tags.append(Movies[0][3])
           
             if(len(temp)!=0):
                 cids.append(temp[0][0])
@@ -40,25 +46,29 @@ class RecommendationEngine:
         faculty=list(set(faculty))
         if(len(faculty)!=0):
             typ=list(set(typ))
-            print("hereis typ")
-            print(typ)
+         
             courseIDs=[]
-            
-            recommend=[]
+            Rtags=[]
+            Movierecommend=[]
             for i in typ:
                 
                 tem = (self.manager.getRecommendedCourses(i))
-                print("tem")
-                print(tem)
+                
                 for j in range(len(tem)):
                     if((tem[j][0] in cids)==False):
                         courseIDs.append(tem[j][0])
-            print("courseids")
-            print(courseIDs)
+      
             for i in courseIDs:        
-               recommend.append(self.moviemanager.getCourseMovieID(i))
-            print(recommend)
-            
+               recommend=self.moviemanager.getCourseMovieID(i)
+               if(len(recommend)!=0):
+                   Rtags.append(recommend[0][1])
+                   Rtags.append(recommend[0][2])
+                   Rtags.append(recommend[0][3])
+                   for k in range(len(recommend)):
+                       if((Rtags[k] in tags)==True):
+                           Movierecommend.append(recommend[0][0])
+                   if(len(Movierecommend)==0):
+                       Movierecommend=recommend
         else:
             #reccommend randomly
             pass
